04b25ee76f01dbae4c04bbed348c228e5014b7c4
Kent Fredric <kentfredric@gmail.com>
2009-10-22 08:22:33 +1300

    Base Commit

---
+++ lib/Dist/Zilla/Plugin/ModuleInstall.pm
@@ -0,0 +1,12 @@
+use strict;
+use warnings;
+
+package Dist::Zilla::Plugin::ModuleInstall;
+
+# ABSTRACT: Build Module::Install based Distributions with Dist::Zilla
+
+use Moose;
+use namespace::autoclean;
+
+1;
+
