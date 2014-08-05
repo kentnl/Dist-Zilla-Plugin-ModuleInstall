ef7b1c11d001bb9e879d474f28190966957c1057
Kent Fredric <kentfredric@gmail.com>
2014-08-05 15:26:57 +1200

    v1.000002
     [00 Trivial]
     - no code changes.
     - Test code updated.
     - Pod::Weaver -SingleEncoding turned on
    
     [Dependencies::Stats]
     - Dependencies changed since 1.000001, see misc/*.deps* for details
     - develop: (suggests: ↑1)
     - test: -3

--- lib/Dist/Zilla/Plugin/ModuleInstall.pm
+++ lib/Dist/Zilla/Plugin/ModuleInstall.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Plugin::ModuleInstall;
 
-our $VERSION = '1.000002';
+our $VERSION = '1.000003';
 
 # ABSTRACT: Build Module::Install based Distributions with Dist::Zilla
 
