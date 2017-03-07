b466003aa7bce56d455c5b2ee9a7e9fac371db57
Kent Fredric <kentfredric@gmail.com>
2017-03-07 21:10:07 +1300

    v1.001002
     - Amended missing changelog entry in a previous release
    
     [Bugfix]
     - Avoid test failures on -Ddefault_inc_excludes_dot
     - Generated Makefile.PL now has a `use lib` stanza to avoid -Ddefault_inc_excludes_dot issues
    
     [Dependencies::Stats]
     - Dependencies changed since 1.001001, see misc/*.deps* for details
     - develop: +1 ↑1 (suggests: ↑2)
     - test: ↓1

--- lib/Dist/Zilla/Plugin/ModuleInstall.pm
+++ lib/Dist/Zilla/Plugin/ModuleInstall.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::ModuleInstall;
 
-our $VERSION = '1.001002';
+our $VERSION = '1.001003';
 
 # ABSTRACT: (DEPRECATED) Build Module::Install based Distributions with Dist::Zilla
 
